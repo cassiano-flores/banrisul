64837498cac50deeceefce4c3ea364df1566dfca
Cassiano Flores <cassiano.flores2424@gmail.com>
2025-12-04 17:13:24 -0300

    function

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,6 +11,25 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            string nome = Console.ReadLine();
+            Console.WriteLine("Nome: " + nome);
+
+            if (string.IsNullOrEmpty(nome))
+            {
+                Console.WriteLine("Nome vazio");
+            }
+            else
+            {
+                ShowMsg("Nome preenchido");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void ShowMsg(string msg)
+        {
+            Console.WriteLine(msg);
         }
     }
 }
