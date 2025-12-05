efb2c828d94cb767ad79414d3f357455be3d0715
Cassiano Flores <cassiano.flores2424@gmail.com>
2025-12-05 18:16:18 -0300

    switch, enum

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,21 +8,30 @@ namespace ConsoleApp
 {
     class Program
     {
+        enum Option { Update = 1, Delete, Create, Read }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
 
-            string nome = Console.ReadLine();
-            Console.WriteLine("Nome: " + nome);
+            //string nome = Console.ReadLine();
+            //Console.WriteLine("Nome: " + nome);
 
-            if (string.IsNullOrEmpty(nome))
-            {
-                Console.WriteLine("Nome vazio");
-            }
-            else
-            {
-                ShowMsg("Nome preenchido");
-            }
+            //if (string.IsNullOrEmpty(nome))
+            //{
+            //    Console.WriteLine("Nome vazio");
+            //}
+            //else
+            //{
+            //    ShowMsg("Nome preenchido");
+            //}
+
+            Console.WriteLine("Options:");
+            Console.WriteLine("1. Update\n2. Delete\n3. Create\n4. Read");
+            int option = int.Parse(Console.ReadLine());
+            Option toOption = (Option) option;
+
+            Console.WriteLine(toOption);
 
             Console.ReadLine();
         }
